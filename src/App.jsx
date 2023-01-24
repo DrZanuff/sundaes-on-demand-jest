d4c8e9672d4c48fffbd9b1e402fc3a55e9fab858
Ricardo <ricardo.machado.nwi@gmail.com>
2023-01-24 00:07:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TestComponent } from './components/TestComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <span>App</span>
+      <TestComponent value="test" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TestComponent/TestComponent.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const TestComponentContainer = styled.div`
+  display: flex;
+` 
