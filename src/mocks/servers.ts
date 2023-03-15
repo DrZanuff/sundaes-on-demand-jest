d8aa71c27648500d65d037a584ab93f0d2d1a375
Ricardo <ricardo.machado.nwi@gmail.com>
2023-03-15 12:14:47 -0300

    github thread fix

--- src/mocks/servers.ts
+++ src/mocks/servers.ts
@@ -3,6 +3,5 @@ import { setupServer } from 'msw/node'
 import { handlers } from './handlers'
 
 // This configures a request mocking server with the given request handlers.
-// export const server = setupServer(...handlers)
-// export const server = setupServer()
+export const server = setupServer(...handlers) // Follow: https://github.com/mswjs/msw/issues/1539#issuecomment-1468498469
 export const serverAddress = 'http://localhost:3030'
